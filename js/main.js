7b99cd4a442867f9668cfdaa3a0cbfa3a7f9ee72
jaswon5791 <jasonwon628@gmail.com>
2016-07-26 22:13:13 -0400

    added info, fixed mobile styles

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 $(function() {
-    var $root = $('html, body');
+    var $root = $('html, body'),
+        $members = $("#team-members");
+
+    if ($(document).width() <= 767) {
+        $members.removeClass('text-left');
+        $members.addClass('text-center');
+    }
+
     $('a').click(function() {
         var href = $.attr(this, 'href');
         $root.animate({
@@ -8,9 +15,19 @@ $(function() {
         return false;
     });
 
-    $('.nav a').on('click', function(){
+    $('.nav a').bind('touchstart', function(){
         if ($(document).width() <= 767){
             $(".navbar-toggle").click();
+        }
+    });
+
+    $(document).bind('touchstart',function (event) {
+        if ($(document).width() <= 767){
+            var clickover = $(event.target);
+            var _opened = $(".navbar-collapse").hasClass("collapse in");
+            if (_opened === true && !clickover.hasClass("navbar-toggle")) {
+                $("button.navbar-toggle").click();
+            }
         }
     });
 })
