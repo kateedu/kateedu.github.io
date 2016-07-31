4317b43d25567e0f6a0c60b4b9705a28da716d14
jaswon5791 <jasonwon628@gmail.com>
2016-07-31 18:33:23 -0400

    v2
    - include bootstrap and jQuery (easier in dev)
    - services section gets descriptions
    - minor color/layout changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(function() {
+
     var $root = $('html, body'),
         $members = $("#team-members");
 
@@ -16,13 +17,48 @@ $(function() {
         return false;
     });
 
-    $('.nav a').bind('touchstart', function(){
+    $('.nav a').on('touchstart', function(){
         if ($(document).width() <= 767){
             $(".navbar-toggle").click();
         }
     });
 
-    $(document).bind('touchstart',function (event) {
+    $('.panel-body.lead ul li').click(function() {
+        $('body').css({
+            top: '-'+$('body').scrollTop()+'px',
+            position: 'fixed'
+        })
+        var title = $(this).children('.service-title').text()
+        var desc = $(this).children('.service-desc').html()
+        if ($(document).width() <= 767){
+            $('.shadow').show().animate({opacity: .4}, 200, function () {
+                $('#desc h1').text(title).siblings('p').html(desc).parent().animate({top: $(window).height()*.2}, 400)
+            });
+        } else {
+            $('.shadow').show().animate({opacity: .4}, 200, function () {
+                $('#desc h1').text(title).siblings('p').html(desc).parent().animate({left: 0}, 400)
+            });
+        }
+    })
+
+    $('.shadow').click(function () {
+        var top = $('body').offset().top*-1
+        $('body').css({
+            top: '',
+            position: ''
+        }).scrollTop(top)
+        if ($(document).width() <= 767) {
+            $('#desc').animate({top: $(window).height()}, 400, function () {
+                $('.shadow').animate({opacity: 0}, 200, function() {$(this).hide()})
+            })
+        } else {
+            $('#desc').animate({left: $(window).width()*-.8}, 400, function () {
+                $('.shadow').animate({opacity: 0}, 200, function() {$(this).hide()})
+            })
+        }
+    })
+
+    $(document).on('touchstart',function (event) {
         if ($(document).width() <= 767){
             var clickover = $(event.target);
             var _opened = $(".navbar-collapse").hasClass("collapse in");
